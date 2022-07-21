16474e17d67b30f957536f7da5a60eec074400f4
0417taehyun <0417taehyun@gmail.com>
2022-07-22 06:40:18 +0900

    :construction: Slack OAuth 2.0

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,8 +1,15 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Query, Request
+
+from src.util import slack
 
 router = APIRouter()
 
 
 @router.post("/slack")
-async def slack_oauth(request: Request):
-    print(request.__dict__)
+async def slack_oauth(
+    request: Request,
+    code: str = Query(..., description="Slack에서 제공해주는 인증을 위한 임시 코드"),
+):
+    worker = slack.Worker()
+    result = worker.oauth_access(auth_code=code)
+    print(result)
