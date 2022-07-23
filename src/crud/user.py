079a1d402b2aaf4b16be3e073d5a458d152f9e98
0417taehyun <0417taehyun@gmail.com>
2022-07-23 19:51:51 +0900

    :construction: Slack OAuth Test

--- src/crud/user.py
+++ src/crud/user.py
@@ -24,7 +24,7 @@ class CRUDUser(CRUDBase[CreateUserModel, UpdateUserModel]):
 
         tokens = await session.find_one({"_id": ObjectId(user_id)})
         print(tokens)
-        for token in tokens["slack"]["accessToken"]:
+        for token in tokens["accessToken"]:
             if token == update_data.slack["accessToken"]:
                 raise Exception("Alrey Exists")
 
