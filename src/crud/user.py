dc5246b69f39d18e78a975cf92f50cd08296dc68
0417taehyun <0417taehyun@gmail.com>
2022-07-23 18:34:01 +0900

    :construction: Slack OAuth Test

--- src/crud/user.py
+++ src/crud/user.py
@@ -1,6 +1,5 @@
 from fastapi import Request
 from fastapi.encoders import jsonable_encoder
-from motor.motor_asyncio import AsyncIOMotorClient
 
 from src.crud.base import CRUDBase
 from src.schema import (
@@ -16,12 +15,12 @@ class CRUDUser(CRUDBase[CreateUserModel, UpdateUserModel]):
         self,
         request: Request,
         user_id: str,
-        platform: Platforms.value,
+        platform: Platforms,
         update_data: SlackIntegrationModel,
     ):
-        session: AsyncIOMotorClient = request.app.db[self.collection]
+        session = request.app.db[self.collection]
 
-        result = await session.update(
+        result = await session.update_one(
             {"_id": user_id},
             {"$push": {platform: jsonable_encoder(update_data)}},
         )
