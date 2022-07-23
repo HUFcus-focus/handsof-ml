b42377e510efe0b09b2ccb5f840e636221eb2f29
0417taehyun <0417taehyun@gmail.com>
2022-07-23 18:55:44 +0900

    :construction: Slack OAuth Test

--- src/crud/user.py
+++ src/crud/user.py
@@ -17,7 +17,7 @@ class CRUDUser(CRUDBase[CreateUserModel, UpdateUserModel]):
 
         result = await session.update_one(
             {"_id": ObjectId(user_id)},
-            {"$push": {jsonable_encoder(update_data)}},
+            {"$push": jsonable_encoder(update_data)},
         )
         print(result.__dict__)
         return result
